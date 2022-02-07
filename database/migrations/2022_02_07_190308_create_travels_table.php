1aadc9513da3624901fe75a2eb098bee925d0963
RobertoVillani <robertovillani58@gmail.com>
2022-02-07 20:14:00 +0100

    first commit

---
+++ database/migrations/2022_02_07_190308_create_travels_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string("departure", 50);
+            $table->string("destination", 50);
+            $table->date('return_date');
+            $table->integer('people_nr');
+            $table->float('price', 8, 2);
+            $table->date('departure_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
---
+++ database/seeds/TravelsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Travel;
+
+class Travels extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 15; $i++) {
+            $travel = new Travel();
+            $travel->departure = $faker->country();
+            $travel->destination = $faker->country();
+            $travel->return_date = $faker->dateTimeThisMonth('+5days');
+            $travel->people_nr = $faker->randomDigit();
+            $travel->price = $faker->randomFloat(2, 50, 1500);
+            $travel->departure_date = $faker->dateTimeThisMonth();
+            $travel->save();
+        }
+    }
+}
